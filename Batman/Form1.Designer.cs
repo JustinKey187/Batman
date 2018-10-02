e406246dd52edbb89c8d0de920c306a07d0acf3f
Jmoney187 <justin.key@outlook.com>
2018-10-02 10:44:47 -0700

    Add project files.

---
+++ Batman/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace Batman
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.p1 = new System.Windows.Forms.PictureBox();
+            this.p2 = new System.Windows.Forms.PictureBox();
+            this.p3 = new System.Windows.Forms.PictureBox();
+            this.p4 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Score:";
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.Transparent;
+            this.player.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.player.Image = global::Batman.Properties.Resources.batman_11;
+            this.player.Location = new System.Drawing.Point(132, 169);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(45, 48);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.player.TabIndex = 1;
+            this.player.TabStop = false;
+            // 
+            // p1
+            // 
+            this.p1.BackColor = System.Drawing.Color.Transparent;
+            this.p1.BackgroundImage = global::Batman.Properties.Resources.platform;
+            this.p1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.p1.Location = new System.Drawing.Point(104, 235);
+            this.p1.Name = "p1";
+            this.p1.Size = new System.Drawing.Size(273, 28);
+            this.p1.TabIndex = 2;
+            this.p1.TabStop = false;
+            this.p1.Tag = "platform";
+            // 
+            // p2
+            // 
+            this.p2.BackColor = System.Drawing.Color.Transparent;
+            this.p2.BackgroundImage = global::Batman.Properties.Resources.platform;
+            this.p2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.p2.Location = new System.Drawing.Point(491, 235);
+            this.p2.Name = "p2";
+            this.p2.Size = new System.Drawing.Size(273, 28);
+            this.p2.TabIndex = 3;
+            this.p2.TabStop = false;
+            this.p2.Tag = "platform";
+            // 
+            // p3
+            // 
+            this.p3.BackColor = System.Drawing.Color.Transparent;
+            this.p3.BackgroundImage = global::Batman.Properties.Resources.platform;
+            this.p3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.p3.Location = new System.Drawing.Point(304, 26);
+            this.p3.Name = "p3";
+            this.p3.Size = new System.Drawing.Size(273, 28);
+            this.p3.TabIndex = 4;
+            this.p3.TabStop = false;
+            this.p3.Tag = "platform";
+            // 
+            // p4
+            // 
+            this.p4.BackColor = System.Drawing.Color.Transparent;
+            this.p4.BackgroundImage = global::Batman.Properties.Resources.platform;
+            this.p4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.p4.Location = new System.Drawing.Point(705, 26);
+            this.p4.Name = "p4";
+            this.p4.Size = new System.Drawing.Size(273, 28);
+            this.p4.TabIndex = 5;
+            this.p4.TabStop = false;
+            this.p4.Tag = "platform";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Batman.Properties.Resources.city;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(784, 261);
+            this.Controls.Add(this.p4);
+            this.Controls.Add(this.p3);
+            this.Controls.Add(this.p2);
+            this.Controls.Add(this.p1);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Batman Gravity Run";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyisdown);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.PictureBox p1;
+        private System.Windows.Forms.PictureBox p2;
+        private System.Windows.Forms.PictureBox p3;
+        private System.Windows.Forms.PictureBox p4;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
